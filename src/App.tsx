9c5a45b2b604c707fc8baf4e7517683cf0661eba
Alexander <SaSkrkr123.com@mail.ru>
2025-05-30 18:02:09 +0300

    start

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+      <div className='osnova'>
+        <header>
+          <div className='header'>
+            <b>Real Madrid vs Barcelona</b>
+          </div>
+        </header>
+      </div>
+  )
+}
+
+export default App
