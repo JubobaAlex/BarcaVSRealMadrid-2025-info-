e5d9e8c017e23135de67d7e75a8d1cd024da74a7
Alexander <SaSkrkr123.com@mail.ru>
2025-05-30 19:19:18 +0300

    create panel

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,13 @@ function App() {
             <b>Real Madrid vs Barcelona</b>
           </div>
         </header>
+        <div className='info'>
+          <div className='chet'>
+            <div>
+              <b>Барселона <img className='ico' src='./public/icoBar.png' /> 4 : 3 Реал Мадрид <img className='ico' src='./public/icoMadr.png' /></b>
+            </div>
+          </div>
+        </div>
       </div>
   )
 }
